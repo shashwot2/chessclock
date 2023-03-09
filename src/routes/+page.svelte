43aa30c0227561c3a890894c5fe8ddc9602879ac
Shashwot KC <shashwot_07@hotmail.com>
2023-03-09 17:19:05 +0700

    Add startTimer function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,20 @@
-
 <script lang="ts">
+    let Lcounter = 500;
+    let interval: ReturnType<typeof setInterval>;
+    let Rcounter = 5;
+    function startTimer() {
+        interval = setInterval(() => {
+            if (Lcounter > 0)
+                Lcounter--;
+            else if (Lcounter == 0)
+                clearInterval(interval)
+        }, 1)
+
+    }
 </script>
 
-<button>
-    red 
+<button on:click={startTimer}>
+    {Lcounter} 
 </button>
 
 <button>
