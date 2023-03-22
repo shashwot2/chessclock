86742509351bca2bceddb34b33fa0f6b83829aa7
Shashwot KC <49743516+shashwot2@users.noreply.github.com>
2023-03-22 10:10:20 +0700

    Workable chess clock without design (#2)
    * Add startTimer function
    
    * Update with timeup alert
    
    * Add switchPlayer function
    
    * Fix bug of time getting faster with click
    
    * Fix minor bug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,53 @@
-
 <script lang="ts">
+	import { onDestroy } from "svelte";
+
+
+    // This is the total number of time alotted to right and left
+    // Player1 is the left player and Player2 is the right player
+    let currentplayer: number = 2;
+    let Lcounter:number = 10;
+    let Rcounter:number = 10;
+    let counterID: ReturnType<typeof setInterval>;
+    function startstopTimer() {
+        clearInterval(counterID)
+        counterID = setInterval(() => {
+            if ((Lcounter > 0) && (currentplayer == 1)) {
+                Lcounter--;
+            }
+            else if ((Lcounter > 0) && (currentplayer == 2)) {
+                Rcounter--;
+            }
+            else if (Lcounter == 0) {
+                clearInterval(counterID)
+                alert("Time is up! right wins!")
+                return                
+            }
+            else if (Rcounter == 0) {
+                clearInterval(counterID)
+                alert("Time is up! left wins!")
+                return
+            }
+        }, 1000)
+        switchPlayers();
+    }
+    function switchPlayers() {
+        if (currentplayer == 1) {
+            currentplayer = 2;
+        }
+        else if (currentplayer == 2) {
+            currentplayer = 1;
+        }
+    }
+
+    onDestroy(() => {
+        clearInterval(counterID);
+    });
 </script>
 
-<button>
-    red 
+<button on:click={startstopTimer}>
+    {Lcounter} 
 </button>
 
-<button>
-    white
+<button on:click={startstopTimer}>
+    {Rcounter}
 </button>
