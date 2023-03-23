c7abf32eaec601c07d8c6fbb10145b05acc006fe
Shashwot KC <49743516+shashwot2@users.noreply.github.com>
2023-03-23 15:07:34 +0700

    Dev (#3)
    * Add startTimer function
    
    * Update with timeup alert
    
    * Add switchPlayer function
    
    * Fix bug of time getting faster with click
    
    * Fix minor bug
    
    * Add basic styling
    
    * Fix minor styling

---
+++ src/routes/utils/secondstominutes.svelte
@@ -0,0 +1,7 @@
+<script context="module" lang="ts">
+	export function convertSecToMin(seconds) {
+		const minutes: number = Math.floor(seconds / 60);
+		const remainingSeconds: number = seconds % 60;
+        return `${minutes.toString().padStart(2,'0')}:${remainingSeconds.toString().padStart(2,'0')}`;
+	}
+</script>
